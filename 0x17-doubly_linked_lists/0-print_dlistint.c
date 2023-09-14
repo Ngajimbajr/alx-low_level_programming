abfb3684d564a4084a29e04734fa0ed2864c42af
Ngajimbajr <ngajimbajr@gmail.com>
2023-09-14 15:02:38 -0700

    0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,15 +9,15 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-    size_t count = 0; /* Initialize count to 0 */
+	size_t count = 0; /* Initialize count to 0 */
 
-    /* Traverse the list and print each node's value */
-    while (h != NULL)
-    {
-        printf("%d\n", h->n);
-        h = h->next;
-        count++;
-    }
+	/* Traverse the list and print each node's value */
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
 
-    return count; /* Return the number of nodes */
+	return (count); /* Return the number of nodes */
 }
